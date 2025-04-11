3cce422ffe6e493bf0ca178d0206fed337276e48
joaosehn2406 <jpssantosos5874@gmail.com>
2025-04-11 14:44:26 -0300

    CategoriaDTO created

---
+++ src/main/java/com/jceco/inventario_api/dto/CategoriaDTO.java
@@ -0,0 +1,22 @@
+package com.jceco.inventario_api.dto;
+
+public class CategoriaDTO {
+
+    private Long id;
+    private String nome;
+
+    public CategoriaDTO() {}
+
+    public CategoriaDTO(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Long getId() { return id; }
+
+    public void setId(Long id) { this.id = id; }
+
+    public String getNome() { return nome; }
+
+    public void setNome(String nome) { this.nome = nome; }
+}
