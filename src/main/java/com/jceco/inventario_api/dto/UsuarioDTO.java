36b4240b8fc8c9b548e9b7e2975cb8753bc7bb24
joaosehn2406 <jpssantosos5874@gmail.com>
2025-04-11 14:48:21 -0300

    UsuarioDTO created

---
+++ src/main/java/com/jceco/inventario_api/dto/UsuarioDTO.java
@@ -0,0 +1,49 @@
+package com.jceco.inventario_api.dto;
+
+import com.jceco.inventario_api.entities.enums.TipoCargo;
+
+public class UsuarioDTO {
+
+    private Long id;
+    private String nome;
+    private String cargo; 
+
+    public UsuarioDTO() {}
+
+    public UsuarioDTO(Long id, String nome, TipoCargo cargoEnum) {
+        this.id = id;
+        this.nome = nome;
+        this.cargo = cargoEnum != null ? cargoEnum.name() : null;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public TipoCargo getCargoEnum() {
+        if (cargo != null) {
+            return TipoCargo.valueOf(cargo);
+        }
+        return null;
+    }
+}
