301c16d99cdf1e7cc4787aa091efce3b6a5024b9
joaosehn2406 <jpssantosos5874@gmail.com>
2025-04-11 11:22:54 -0300

    Project Created

---
+++ src/test/java/com/jceco/inventario_api/InventarioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jceco.inventario_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
