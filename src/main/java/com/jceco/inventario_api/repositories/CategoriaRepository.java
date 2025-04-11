551c91f4e81702157c21a5db0b7c33f85d9d6ba0
joaosehn2406 <jpssantosos5874@gmail.com>
2025-04-11 13:36:06 -0300

    CategoriaRepository created

---
+++ src/main/java/com/jceco/inventario_api/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.jceco.inventario_api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jceco.inventario_api.entities.Categoria;
+
+
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+
+}
